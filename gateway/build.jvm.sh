d860ac3f7de6f1e3ae328484365d03ec6a08cb08
vicbap <vicbap@gmail.com>
2021-05-20 12:25:52 +0200

    name

--- gateway/build.jvm.sh
+++ gateway/build.jvm.sh
@@ -2,6 +2,8 @@ chmod +x mvn*
 mvn -N io.takari:maven:wrapper
 mvn clean package
 # -Pnative -Dquarkus.native.container-build=true -Dquarkus.native.container-runtime=docker
-docker rmi quay.io/qiotcovid19/edge-service:2.0.0-aarch64-jvm
-docker build -f src/main/docker/Dockerfile.jvm -t quay.io/qiotcovid19/edge-service:2.0.0-aarch64-jvm .
-docker push quay.io/qiotcovid19/edge-service:2.0.0-aarch64-jvm
+docker rmi quay.io/cesar_getronics/qiot-edge-service:2.0.0-aarch64-jvm
+docker build -f src/main/docker/Dockerfile.jvm -t quay.io/cesar_getronics/qiot-edge-service:2.0.0-aarch64-jvm .
+docker push quay.io/cesar_getronics/qiot-edge-service:2.0.0-aarch64-jvm
+
+
